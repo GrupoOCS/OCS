95a699299c763eaf3067fb27669345fe86a77e44
adriiiana <adriiiherrera@gmail.com>
2015-11-27 00:06:40 -0600

    Adri

--- ventas.php
+++ ventas.php
@@ -2,15 +2,56 @@
 	<!--.............................TERMINA NAVEGACIÓN...............................-->
 	<div class="contenido">
 
-		<div class="producto"><img class="producto" src="Productos/1comp.jpg"></div>
-		<div class="producto"><img class="producto" src="Productos/2comp.jpg"></div>
-		<div class="producto"><img class="producto" src="Productos/3comp.jpg"></div>
-		<div class="producto"><img class="producto" src="Productos/4comp.jpg"></div>
-		<div class="producto"><img class="producto" src="Productos/5comp.jpg"></div>
-		<div class="producto"><img class="producto" src="Productos/6comp.jpg"></div>
-		<div class="producto"><img class="producto" src="Productos/7comp.jpg"></div>
-		<div class="producto"><img class="producto" src="Productos/9comp.jpg"></div>
-		
+		<div class="producto">
+			<img class="producto" src="Productos/1comp.jpg">
+			<div class="nombre_producto">Wireless TP-link</div>
+			<div class="precio_producto">$550.00</div>
+			<a href="#	" class="agrega_carrito"><img class="add_car" src="iconos/agregar.png"></a>
+		</div>
+		<div class="producto">
+			<img class="producto" src="Productos/2comp.jpg">
+			<div class="nombre_producto">Wireless TP-link</div>
+			<div class="precio_producto">$550.00</div>
+			<a href="#	" class="agrega_carrito"><img class="add_car" src="iconos/agregar.png"></a>
+		</div>
+		<div class="producto">
+			<img class="producto" src="Productos/3comp.jpg">
+			<div class="nombre_producto">Wireless TP-link</div>
+			<div class="precio_producto">$550.00</div>
+			<a href="#	" class="agrega_carrito"><img class="add_car" src="iconos/agregar.png"></a>
+		</div>
+		<div class="producto">
+			<img class="producto" src="Productos/4comp.jpg">
+			<div class="nombre_producto">Wireless TP-link</div>
+			<div class="precio_producto">$550.00</div>
+			<a href="#	" class="agrega_carrito"><img class="add_car" src="iconos/agregar.png"></a>
+		</div>
+		<div class="producto">
+			<img class="producto" src="Productos/5comp.jpg">
+			<div class="nombre_producto">Wireless TP-link</div>
+			<div class="precio_producto">$550.00</div>
+			<a href="#	" class="agrega_carrito"><img class="add_car" src="iconos/agregar.png"></a>
+		</div>
+		<div class="producto">
+			<img class="producto" src="Productos/6comp.jpg">
+			<div class="nombre_producto">Wireless TP-link</div>
+			<div class="precio_producto">$550.00</div>
+			<a href="#	" class="agrega_carrito"><img class="add_car" src="iconos/agregar.png"></a>
+		</div>
+		<div class="producto">
+			<img class="producto" src="Productos/7comp.jpg">
+			<div class="nombre_producto">Wireless TP-link</div>
+			<div class="precio_producto">$550.00</div>
+			<a href="#	" class="agrega_carrito"><img class="add_car" src="iconos/agregar.png"></a>
+		</div>
+		<div class="producto">
+			<img class="producto" src="Productos/9comp.jpg">
+			<div class="nombre_producto">Wireless TP-link</div>
+			<div class="precio_producto">$550.00</div>
+			<a href="#	" class="agrega_carrito"><img class="add_car" src="iconos/agregar.png"></a>
+		</div>
+
+
 	</div>
 	<!--................................................................. -->
 <?php include('pie_pagina.php'); ?>
